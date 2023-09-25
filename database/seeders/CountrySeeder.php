2a1dcbdad5b4d9b16782d4061687e6d6143be69e
Juan Cruz <juancruzf017@gmail.com>
2023-09-25 20:16:18 -0300

    feat: country: create migration, model and seeder

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CountrySeeder extends Seeder
+{
+    private array $lstCountryNames = ["Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Costa Rica", "Cuba", "Ecuador", "El Salvador", "Guatemala", "Guyana", "Haití", "Honduras", "México", "Nicaragua", "Panamá", "Paraguay", "Perú", "República Dominicana", "Surinam", "Uruguay", "Venezuela"];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->lstCountryNames as $name) {
+            Country::create([
+                'name' => $name
+            ]);
+        }
+    }
+}
